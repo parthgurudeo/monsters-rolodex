24f7176e1930c359d6e57515855b98119668c62d
parthgurudeo <parth@wecp.in>
2023-04-23 17:33:07 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchString:''
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users,
+            filteredMonsters:users };
+          },
+          () => {
+            
+          }
+        )
+      );
+  }
+onSearchChange=(event)=>{
+    const searchString=event.target.value
+      this.setState(()=>{return {searchString}});
+    }
+  
+
+  render() {
+    const {monsters,searchString}=this.state;
+    const{onSearchChange}=this
+    const filteredMonsters=monsters.filter((item)=> item.name.toLocaleLowerCase().includes(searchString))
+    return (
+      <div className="App">
+        <div>
+
+  <input name="searchMonsters" type="search" placeholder="search here" onChange={onSearchChange} />
+        </div>
+        {
+        
+       filteredMonsters.map((item) => {
+          return <h1 key={item.id}>{item.name}</h1>;
+        })}
+      </div>
+    );
+  }
 }
-
 export default App;
